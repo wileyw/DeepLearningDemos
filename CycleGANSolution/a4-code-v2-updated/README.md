2f0ef01fce1ae1e1824529fca5cf26a4bffcb813
Michael Wang <mswang12@gmail.com>
2019-10-31 20:35:40 -0700

    Add CycleGANSolution

---
+++ CycleGANSolution/a4-code-v2-updated/README.md
@@ -0,0 +1,15 @@
+Vanilla GAN
+## 1. Run the model checker
+```
+python3 model_checker.py
+```
+
+## 2. Run vanilla GAN
+```
+python3 vanilla_gan.py
+```
+
+## 3. Run Cycle GAN
+```
+python3 cycle_gan.py --use_cycle_consistency_loss
+```
---
+++ CycleGANSolution/a4-code-v2-updated/data_loader.py
@@ -0,0 +1,28 @@
+import os
+
+import torch
+from torch.utils.data import DataLoader
+
+from torchvision import datasets
+from torchvision import transforms
+
+
+def get_emoji_loader(emoji_type, opts):
+    """Creates training and test data loaders.
+    """
+    transform = transforms.Compose([
+                    transforms.Scale(opts.image_size),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                ])
+
+    train_path = os.path.join('./emojis', emoji_type)
+    test_path = os.path.join('./emojis', 'Test_{}'.format(emoji_type))
+
+    train_dataset = datasets.ImageFolder(train_path, transform)
+    test_dataset = datasets.ImageFolder(test_path, transform)
+
+    train_dloader = DataLoader(dataset=train_dataset, batch_size=opts.batch_size, shuffle=True, num_workers=opts.num_workers)
+    test_dloader = DataLoader(dataset=test_dataset, batch_size=opts.batch_size, shuffle=False, num_workers=opts.num_workers)
+
+    return train_dloader, test_dloader
