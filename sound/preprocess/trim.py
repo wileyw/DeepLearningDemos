013a216062d355f2b96b445aa15a581c81497ec2
Michael Wang <mswang12@gmail.com>
2021-02-12 14:55:38 -0800

    Make trim.py to a script

---
+++ sound/preprocess/trim.py
@@ -0,0 +1,28 @@
+"""
+Split .wav files with ffmpeg:
+NAME=name.wav
+ffmpeg -i $NAME.wav -f segment -segment_time 2 -c copy one_second/$NAME%03d.wav
+
+https://petewarden.com/2017/07/17/a-quick-hack-to-align-single-word-audio-recordings/
+
+NOTE: Run make from the extract_loudest_section repo before running this script
+"""
+import glob
+import os
+import sys
+
+def main():
+    if len(sys.argv) < 3:
+        print('Usage: python3 trim.py INPUT_DIR OUTPUT_DIR')
+        return
+
+    if not os.path.exists(sys.argv[2]):
+        os.makedirs(sys.argv[2])
+
+    file_names = glob.glob(os.path.join(sys.argv[1], '*.wav'))
+    for filename in file_names:
+        print(filename)
+        os.system('/tmp/extract_loudest_section/gen/bin/extract_loudest_section {} {}'.format(filename, sys.argv[2]))
+
+if __name__ == '__main__':
+    main()
