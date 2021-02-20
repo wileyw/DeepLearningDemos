30b8ea65a3ac36b51a168e58b95569985dc97bc9
Michael Wang <mswang12@gmail.com>
2021-02-19 22:42:51 -0800

    Add RL example

---
+++ ReinforcmentLearning/simple_example.py
@@ -0,0 +1,12 @@
+import gym
+env = gym.make("MsPacman-ram-v0")
+observation = env.reset()
+import time
+for _ in range(1000):
+  env.render()
+  action = env.action_space.sample() # your agent here (this takes random actions)
+  observation, reward, done, info = env.step(action)
+
+  if done:
+    observation = env.reset()
+env.close()
